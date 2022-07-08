05b69f38354bb8f7df8cc828b74fe140037b43d1
unknown <33962956+EnFlorian@users.noreply.github.com>
2022-07-08 22:47:53 +0200

    Style: Added image

--- src/components/elements/InfoCard/InfoCard.tsx
+++ src/components/elements/InfoCard/InfoCard.tsx
@@ -3,16 +3,17 @@ import React from "react";
 import styles from "./InfoCard.module.css";
 const InfoCard = () => {
   return (
-    <article>
+    <article className={`container ${styles.infoCard}`}>
       <div className={styles.leftContent}>
         <div className={styles.imageWrapper}>
-          <Image src="/assets/images/doc1.jpg" alt="info-image" className={styles.image} width="320px" height="400px" />
+          <img src="/assets/images/doc1.jpg" alt="info-image" className={styles.image} width="320px" height="400px" />
         </div>
       </div>
       <section className={styles.rightContent}>
         <h2 className={styles.title}> 24/7 Emergency Service</h2>
         <p className={styles.description}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisi eu consectetur consectetur.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisi eu consectetur consectetur. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisi eu consectetur consectetur.
         </p>
         <button className={styles.button}> Read More </button>
       </section>

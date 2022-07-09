7f188124e9f4e0b72826cbb34195dd055a43c4a7
unknown <33962956+EnFlorian@users.noreply.github.com>
2022-07-09 18:07:53 +0200

    Feat: Added section header

--- src/components/elements/SectionHeader/SectionHeader.tsx
+++ src/components/elements/SectionHeader/SectionHeader.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import styles from "./SectionHeader.module.css";
 const SectionHeader = () => {
-  return <div>SectionHeader</div>;
+  return (
+    <section className={styles.sectionHeader}>
+      <h2 className={styles.title}>
+        Emergency <span className={styles.accent}>Services</span>
+      </h2>
+      <p className={styles.description}>
+        lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisi eu consectetur consectetur. Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+    </section>
+  );
 };
 
 export default SectionHeader;
--- src/components/layout/ServicesSection/ServicesSection.tsx
+++ src/components/layout/ServicesSection/ServicesSection.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Slider from "react-slick";
+import { SectionHeader } from "../../elements";
 import styles from "./ServicesSection.module.css";
 const ServicesSection = () => {
-  return <div>ServicesSection</div>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    pauseOnHover: true,
+  };
+
+  return (
+    <section className={`container ${styles.wrapper}`}>
+      <SectionHeader />
+
+      <div className={styles.services}>
+        <Slider {...settings}></Slider>
+      </div>
+    </section>
+  );
 };
 
 export default ServicesSection;

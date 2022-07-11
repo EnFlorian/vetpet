4f80e918b9d29f8222034c0cc8b0e051c1471664
unknown <33962956+EnFlorian@users.noreply.github.com>
2022-07-11 16:02:56 +0200

    Feat: Added custom page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import {
   BlogSection,
   ContactSection,
@@ -13,15 +14,21 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <HeroSection />
-      <InfoSection />
-      <ServicesSection />
-      <StatsSection />
-      <TeamSection />
-      <BlogSection />
-      <NewsletterSection />
-    </div>
+    <>
+      <Head>
+        <title>VetPet</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className={styles.container}>
+        <HeroSection />
+        <InfoSection />
+        <ServicesSection />
+        <StatsSection />
+        <TeamSection />
+        <BlogSection />
+        <NewsletterSection />
+      </div>
+    </>
   );
 };
 

9f98f0fd867ac7efdef9cbb088f36ba85ed06dac
unknown <33962956+EnFlorian@users.noreply.github.com>
2022-07-11 13:39:23 +0200

    Feat: Added 404 page

---
+++ src/pages/404/404.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+import styles from "./404.module.css";
+const NotFound = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.text}>Page was not found</p>
+      <Link href="/">
+        <a className={styles.link}>Back Home</a>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;

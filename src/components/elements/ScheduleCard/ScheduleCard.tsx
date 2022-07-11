f67a0ffc750302c7df738621da268cc2f3039b9f
unknown <33962956+EnFlorian@users.noreply.github.com>
2022-07-12 01:43:27 +0200

    Refactor: Moved data to its own file

--- src/components/elements/ScheduleCard/ScheduleCard.tsx
+++ src/components/elements/ScheduleCard/ScheduleCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./ScheduleCard.module.css";
+import schedule from "../../../data/schedule";
 const ScheduleCard = () => {
   return (
     <article className={`container ${styles.scheduleCard}`}>
@@ -12,31 +13,31 @@ const ScheduleCard = () => {
         <section className={styles.timeTable}>
           <div className={styles.row}>
             <p className={styles.day}>Monday </p>
-            <p className={styles.time}>8:00 - 20:00</p>
+            <p className={styles.time}>{schedule.monday}</p>
           </div>
           <div className={styles.row}>
             <span className={styles.day}>Tuesday </span>
-            <span className={styles.time}>8:00 - 20:00</span>
+            <span className={styles.time}>{schedule.tuesday}</span>
           </div>
           <div className={styles.row}>
             <span className={styles.day}>Wednesday </span>
-            <span className={styles.time}>8:00 - 20:00</span>
+            <span className={styles.time}>{schedule.wednesday}</span>
           </div>
           <div className={styles.row}>
             <span className={styles.day}>Thursday </span>
-            <span className={styles.time}>8:00 - 20:00</span>
+            <span className={styles.time}>{schedule.thursday}</span>
           </div>
           <div className={styles.row}>
             <span className={styles.day}>Friday </span>
-            <span className={styles.time}>8:00 - 20:00</span>
+            <span className={styles.time}>{schedule.friday}</span>
           </div>
           <div className={styles.row}>
             <span className={styles.day}>Saturday </span>
-            <span className={styles.time}>8:00 - 12:00</span>
+            <span className={styles.time}>{schedule.saturday}</span>
           </div>
           <div className={styles.row}>
             <span className={styles.day}>Sunday </span>
-            <span className={styles.time}>8:00 - 17:00</span>
+            <span className={styles.time}>{schedule.sunday}</span>
           </div>
         </section>
       </section>
---
+++ src/data/schedule.ts
@@ -0,0 +1,11 @@
+const schedule = {
+  monday: "08:00 - 18:00",
+  tuesday: "08:00 - 18:00",
+  wednesday: "08:00 - 18:00",
+  thursday: "08:00 - 18:00",
+  friday: "08:00 - 18:00",
+  saturday: "08:00 - 12:00",
+  sunday: "08:00 - 12:00",
+};
+
+export default schedule;

8a53a4be36b5dbbf1f8dde78a20d03895219fab3
unknown <33962956+EnFlorian@users.noreply.github.com>
2022-07-20 14:35:54 +0200

    Refactor: Cleaned up render function

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactText } from "react";
 import styles from "./Navbar.module.css";
 import { FaPaw } from "react-icons/fa";
 import { navigation } from "../../../data/navigation";
@@ -9,6 +9,14 @@ import { Link as ScrollLink } from "react-scroll";
 const Navbar = () => {
   const { setIsMobileMenuOpen } = useUIContext();
 
+  const links = navigation.map((link, index: number) => (
+    <li className={styles.link} key={index}>
+      <ScrollLink smooth={true} spy={true} to={link.path} offset={link.offSet}>
+        {link.name}
+      </ScrollLink>
+    </li>
+  ));
+
   return (
     <div className={styles.navbar}>
       <div className={`container ${styles.wrapper}`}>
@@ -17,13 +25,7 @@ const Navbar = () => {
           <h1>VetPet</h1>
         </div>
         <ul className={styles.menu}>
-          {navigation.map((link, index: number) => (
-            <li className={styles.link} key={index}>
-              <ScrollLink smooth={true} spy={true} to={link.path} offset={link.offSet}>
-                {link.name}
-              </ScrollLink>
-            </li>
-          ))}
+          {links}
           <li className={styles.mobileMenu} onClick={() => setIsMobileMenuOpen(true)}>
             <GiHamburgerMenu />
           </li>
